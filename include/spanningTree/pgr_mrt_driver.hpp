165a43f3b6bbc078716046efdea6d9b74259455f
prakashupes <85prakash2017@gmail.com>
2020-07-06 01:07:06 +0530

    [mrt] cpp header driver

--- include/spanningTree/pgr_mrt_driver.hpp
+++ include/spanningTree/pgr_mrt_driver.hpp
@@ -1,5 +1,5 @@
 /*PGR-GNU*****************************************************************
-File: pgr_mrt_driver.hpp
+File: pgr_LTDTree.hpp
 
 Generated with Template by:
 Copyright (c) 2015 pgRouting developers
@@ -37,75 +37,11 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include "cpp_common/pgr_messages.h"
 #include <iostream>
 #include <algorithm>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/dominator_tree.hpp>
+#include <boost/property_map/property_map.hpp>
+#include <boost/property_map/vector_property_map.hpp>
+#include <boost/type_traits.hpp>
 
-#include "c_types/pgr_mrt_rt.h"
-#include <boost/graph/two_graphs_common_spanning_trees.hpp>
-
-#include <boost/graph/depth_first_search.hpp>
-
-using namespace boost;
-using namespace  std;
-
-namespace pgrouting {
-    namespace functions {
-    /**TODO***
-    1. Return proper results
-    2. SOlve all the errors
-    *******/
-        
-
-        template<class Graph>
-        class Pgr_mrt : public pgrouting::Pgr_messages {
-        public:
-            std::vector<pgr_mrt_rt> results;
-            typedef typename Graph::E E;
-            typedef typename Graph::E_i E_i;
-            typedef typename Graph::V V;
-
-            std::vector<pgr_mrt_rt> pgr_mrt(
-                Graph &graph_1,
-                Graph &graph_2,
-                bool directed
-
-                    ){
-                log<<"Inside main boost driver"<<endl;
-                if(directed)
-                {
-                    //log<<"Edge "<<E<<endl;
-
-
-                    log<<"In directed graph total edges G1 "<<graph_1.num_edges()<<endl;
-                    log<<"In directed graph total edges G2 "<<graph_2.num_edges()<<endl;
-                    log<<"Need coding"<<endl;
-                 
-                }
-                else
-                {/*
-                
-                    log<<"Working with undirected graph"<<endl;
-
-                    std::vector<bool> inL;
-
-                    std::vector< std::vector<bool> > coll;
-                    boost::tree_collector<
-                            std::vector< std::vector<bool> >,
-                            std::vector<bool>
-                    > tree_collector(coll);
-                    boost::two_graphs_common_spanning_trees
-                            (
-                                    graph_1.graph,
-                                    graph_2.graph,
-                                    tree_collector,
-                                    inL
-                            );
-                   */         
-                           
-                }
-                return results;
-
-            }
-        };
-    }
-}
 
 #endif  // INCLUDE_PGR_MRT_DRIVER_HPP
