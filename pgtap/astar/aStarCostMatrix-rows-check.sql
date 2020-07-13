224157e25f85fd492a7373d202695ecfcc51f7dd
Ashish Kumar <ashishkr23438@gmail.com>
2020-07-13 19:16:18 +0530

    [pgtap] Splitted astar test into three files

--- pgtap/astar/aStarCostMatrix-rows-check.sql
+++ pgtap/astar/aStarCostMatrix-rows-check.sql
@@ -6,59 +6,57 @@ SELECT plan(20);
 -- Check whether the same set of rows are returned always
 
 PREPARE expectedOutputDirected AS
-SELECT * FROM pgr_astar(
+SELECT * FROM pgr_aStarCostMatrix(
     'SELECT id, source, target, cost, reverse_cost, x1, y1, x2, y2
     FROM edge_table
     ORDER BY id',
-    ARRAY[7, 2], ARRAY[3, 12]
+    (SELECT array_agg(id) FROM edge_table_vertices_pgr)
 );
 
 PREPARE descendingOrderDirected AS
-SELECT * FROM pgr_astar(
+SELECT * FROM pgr_aStarCostMatrix(
     'SELECT id, source, target, cost, reverse_cost, x1, y1, x2, y2
     FROM edge_table
     ORDER BY id DESC',
-    ARRAY[7, 2], ARRAY[3, 12]
+    (SELECT array_agg(id) FROM edge_table_vertices_pgr)
 );
 
 PREPARE randomOrderDirected AS
-SELECT * FROM pgr_astar(
+SELECT * FROM pgr_aStarCostMatrix(
     'SELECT id, source, target, cost, reverse_cost, x1, y1, x2, y2
     FROM edge_table
     ORDER BY RANDOM()',
-    ARRAY[7, 2], ARRAY[3, 12]
+    (SELECT array_agg(id) FROM edge_table_vertices_pgr)
 );
 
 PREPARE expectedOutputUndirected AS
-SELECT * FROM pgr_astar(
+SELECT * FROM pgr_aStarCostMatrix(
     'SELECT id, source, target, cost, reverse_cost, x1, y1, x2, y2
     FROM edge_table
     ORDER BY id',
-    ARRAY[7, 2], ARRAY[3, 12],
+    (SELECT array_agg(id) FROM edge_table_vertices_pgr),
     directed => false
 );
 
 PREPARE descendingOrderUndirected AS
-SELECT * FROM pgr_astar(
+SELECT * FROM pgr_aStarCostMatrix(
     'SELECT id, source, target, cost, reverse_cost, x1, y1, x2, y2
     FROM edge_table
     ORDER BY id DESC',
-    ARRAY[7, 2], ARRAY[3, 12],
+    (SELECT array_agg(id) FROM edge_table_vertices_pgr),
     directed => false
 );
 
 PREPARE randomOrderUndirected AS
-SELECT * FROM pgr_astar(
+SELECT * FROM pgr_aStarCostMatrix(
     'SELECT id, source, target, cost, reverse_cost, x1, y1, x2, y2
     FROM edge_table
     ORDER BY RANDOM()',
-    ARRAY[7, 2], ARRAY[3, 12],
+    (SELECT array_agg(id) FROM edge_table_vertices_pgr),
     directed => false
 );
 
 SELECT SETSEED(1);
-
-SELECT todo_start('Fix the code to return same set of rows');
 
 SELECT set_eq('expectedOutputDirected', 'descendingOrderDirected', '1: Should return same set of rows');
 SELECT set_eq('expectedOutputDirected', 'randomOrderDirected', '2: Should return same set of rows');
@@ -66,27 +64,25 @@ SELECT set_eq('expectedOutputDirected', 'randomOrderDirected', '3: Should return
 SELECT set_eq('expectedOutputDirected', 'randomOrderDirected', '4: Should return same set of rows');
 SELECT set_eq('expectedOutputDirected', 'randomOrderDirected', '5: Should return same set of rows');
 
+SELECT set_eq('expectedOutputUndirected', 'descendingOrderUndirected', '6: Should return same set of rows');
+SELECT set_eq('expectedOutputUndirected', 'randomOrderUndirected', '7: Should return same set of rows');
+SELECT set_eq('expectedOutputUndirected', 'randomOrderUndirected', '8: Should return same set of rows');
+SELECT set_eq('expectedOutputUndirected', 'randomOrderUndirected', '9: Should return same set of rows');
+SELECT set_eq('expectedOutputUndirected', 'randomOrderUndirected', '10: Should return same set of rows');
+
+UPDATE edge_table SET cost = cost + 0.001 * id * id, reverse_cost = reverse_cost + 0.001 * id * id;
+
+SELECT set_eq('expectedOutputDirected', 'descendingOrderDirected', '11: Should return same set of rows');
+SELECT set_eq('expectedOutputDirected', 'randomOrderDirected', '12: Should return same set of rows');
+SELECT set_eq('expectedOutputDirected', 'randomOrderDirected', '13: Should return same set of rows');
+SELECT set_eq('expectedOutputDirected', 'randomOrderDirected', '14: Should return same set of rows');
+SELECT set_eq('expectedOutputDirected', 'randomOrderDirected', '15: Should return same set of rows');
+
 SELECT set_eq('expectedOutputUndirected', 'descendingOrderUndirected', '16: Should return same set of rows');
 SELECT set_eq('expectedOutputUndirected', 'randomOrderUndirected', '17: Should return same set of rows');
 SELECT set_eq('expectedOutputUndirected', 'randomOrderUndirected', '18: Should return same set of rows');
 SELECT set_eq('expectedOutputUndirected', 'randomOrderUndirected', '19: Should return same set of rows');
 SELECT set_eq('expectedOutputUndirected', 'randomOrderUndirected', '20: Should return same set of rows');
-
-SELECT todo_end();
-
-UPDATE edge_table SET cost = cost + 0.001 * id * id, reverse_cost = reverse_cost + 0.001 * id * id;
-
-SELECT set_eq('expectedOutputDirected', 'descendingOrderDirected', '31: Should return same set of rows');
-SELECT set_eq('expectedOutputDirected', 'randomOrderDirected', '32: Should return same set of rows');
-SELECT set_eq('expectedOutputDirected', 'randomOrderDirected', '33: Should return same set of rows');
-SELECT set_eq('expectedOutputDirected', 'randomOrderDirected', '34: Should return same set of rows');
-SELECT set_eq('expectedOutputDirected', 'randomOrderDirected', '35: Should return same set of rows');
-
-SELECT set_eq('expectedOutputUndirected', 'descendingOrderUndirected', '46: Should return same set of rows');
-SELECT set_eq('expectedOutputUndirected', 'randomOrderUndirected', '47: Should return same set of rows');
-SELECT set_eq('expectedOutputUndirected', 'randomOrderUndirected', '48: Should return same set of rows');
-SELECT set_eq('expectedOutputUndirected', 'randomOrderUndirected', '49: Should return same set of rows');
-SELECT set_eq('expectedOutputUndirected', 'randomOrderUndirected', '50: Should return same set of rows');
 
 SELECT * FROM finish();
 ROLLBACK;
