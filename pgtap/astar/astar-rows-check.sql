764e9c1434fd3fd28de67caa8ec79bd5ec3d9012
Ashish Kumar <ashishkr23438@gmail.com>
2020-07-15 00:08:57 +0530

    [pgtap] Renamed query number

--- pgtap/astar/astar-rows-check.sql
+++ pgtap/astar/astar-rows-check.sql
@@ -66,27 +66,27 @@ SELECT set_eq('expectedOutputDirected', 'randomOrderDirected', '3: Should return
 SELECT set_eq('expectedOutputDirected', 'randomOrderDirected', '4: Should return same set of rows');
 SELECT set_eq('expectedOutputDirected', 'randomOrderDirected', '5: Should return same set of rows');
 
-SELECT set_eq('expectedOutputUndirected', 'descendingOrderUndirected', '16: Should return same set of rows');
-SELECT set_eq('expectedOutputUndirected', 'randomOrderUndirected', '17: Should return same set of rows');
-SELECT set_eq('expectedOutputUndirected', 'randomOrderUndirected', '18: Should return same set of rows');
-SELECT set_eq('expectedOutputUndirected', 'randomOrderUndirected', '19: Should return same set of rows');
-SELECT set_eq('expectedOutputUndirected', 'randomOrderUndirected', '20: Should return same set of rows');
+SELECT set_eq('expectedOutputUndirected', 'descendingOrderUndirected', '6: Should return same set of rows');
+SELECT set_eq('expectedOutputUndirected', 'randomOrderUndirected', '7: Should return same set of rows');
+SELECT set_eq('expectedOutputUndirected', 'randomOrderUndirected', '8: Should return same set of rows');
+SELECT set_eq('expectedOutputUndirected', 'randomOrderUndirected', '9: Should return same set of rows');
+SELECT set_eq('expectedOutputUndirected', 'randomOrderUndirected', '10: Should return same set of rows');
 
 SELECT todo_end();
 
 UPDATE edge_table SET cost = cost + 0.001 * id * id, reverse_cost = reverse_cost + 0.001 * id * id;
 
-SELECT set_eq('expectedOutputDirected', 'descendingOrderDirected', '31: Should return same set of rows');
-SELECT set_eq('expectedOutputDirected', 'randomOrderDirected', '32: Should return same set of rows');
-SELECT set_eq('expectedOutputDirected', 'randomOrderDirected', '33: Should return same set of rows');
-SELECT set_eq('expectedOutputDirected', 'randomOrderDirected', '34: Should return same set of rows');
-SELECT set_eq('expectedOutputDirected', 'randomOrderDirected', '35: Should return same set of rows');
+SELECT set_eq('expectedOutputDirected', 'descendingOrderDirected', '11: Should return same set of rows');
+SELECT set_eq('expectedOutputDirected', 'randomOrderDirected', '12: Should return same set of rows');
+SELECT set_eq('expectedOutputDirected', 'randomOrderDirected', '13: Should return same set of rows');
+SELECT set_eq('expectedOutputDirected', 'randomOrderDirected', '14: Should return same set of rows');
+SELECT set_eq('expectedOutputDirected', 'randomOrderDirected', '15: Should return same set of rows');
 
-SELECT set_eq('expectedOutputUndirected', 'descendingOrderUndirected', '46: Should return same set of rows');
-SELECT set_eq('expectedOutputUndirected', 'randomOrderUndirected', '47: Should return same set of rows');
-SELECT set_eq('expectedOutputUndirected', 'randomOrderUndirected', '48: Should return same set of rows');
-SELECT set_eq('expectedOutputUndirected', 'randomOrderUndirected', '49: Should return same set of rows');
-SELECT set_eq('expectedOutputUndirected', 'randomOrderUndirected', '50: Should return same set of rows');
+SELECT set_eq('expectedOutputUndirected', 'descendingOrderUndirected', '16: Should return same set of rows');
+SELECT set_eq('expectedOutputUndirected', 'randomOrderUndirected', '17: Should return same set of rows');
+SELECT set_eq('expectedOutputUndirected', 'randomOrderUndirected', '18: Should return same set of rows');
+SELECT set_eq('expectedOutputUndirected', 'randomOrderUndirected', '19: Should return same set of rows');
+SELECT set_eq('expectedOutputUndirected', 'randomOrderUndirected', '20: Should return same set of rows');
 
 SELECT * FROM finish();
 ROLLBACK;
