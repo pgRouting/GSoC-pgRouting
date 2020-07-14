02d5b0a4b6eb26cf4d798de577f6941ee85b0a2a
Ashish Kumar <ashishkr23438@gmail.com>
2020-07-14 10:34:06 +0530

    [pgtap] Added rows check for chinese postman family

---
+++ pgtap/chinese/chinesePostman/chinesePostman-rows-check.sql
@@ -0,0 +1,46 @@
+\i setup.sql
+
+SELECT plan(5);
+
+SET extra_float_digits = -3;
+
+-- Check whether the same set of rows are returned always
+
+PREPARE expectedOutput AS
+SELECT * FROM pgr_chinesePostman(
+    'SELECT id, source, target, cost, reverse_cost
+    FROM edge_table
+    WHERE id < 17
+    ORDER BY id'
+);
+
+PREPARE descendingOrder AS
+SELECT * FROM pgr_chinesePostman(
+    'SELECT id, source, target, cost, reverse_cost
+    FROM edge_table
+    WHERE id < 17
+    ORDER BY id DESC'
+);
+
+PREPARE randomOrder AS
+SELECT * FROM pgr_chinesePostman(
+    'SELECT id, source, target, cost, reverse_cost
+    FROM edge_table
+    WHERE id < 17
+    ORDER BY RANDOM()'
+);
+
+SELECT SETSEED(1);
+
+SELECT todo_start('Fix the code to return same set of rows');
+
+SELECT set_eq('expectedOutput', 'descendingOrder', '1: Should return same set of rows');
+SELECT set_eq('expectedOutput', 'randomOrder', '2: Should return same set of rows');
+SELECT set_eq('expectedOutput', 'randomOrder', '3: Should return same set of rows');
+SELECT set_eq('expectedOutput', 'randomOrder', '4: Should return same set of rows');
+SELECT set_eq('expectedOutput', 'randomOrder', '5: Should return same set of rows');
+
+SELECT todo_end();
+
+SELECT * FROM finish();
+ROLLBACK;
