6f1c94da1bc37c76fa90ccd324f9a8e971d4567f
Ashish Kumar <ashishkr23438@gmail.com>
2020-07-14 12:09:07 +0530

    [pgtap] Added rows check for DAG Shortest Path

---
+++ pgtap/dagShortestPath/dagShortestPath-rows-check.sql
@@ -0,0 +1,53 @@
+\i setup.sql
+
+SELECT plan(10);
+
+
+-- Check whether the same set of rows are returned always
+
+PREPARE expectedOutput AS
+SELECT * FROM pgr_dagShortestPath(
+    'SELECT id, source, target, cost
+    FROM edge_table
+    ORDER BY id',
+    ARRAY[10, 2], ARRAY[11, 12]
+);
+
+PREPARE descendingOrder AS
+SELECT * FROM pgr_dagShortestPath(
+    'SELECT id, source, target, cost
+    FROM edge_table
+    ORDER BY id DESC',
+    ARRAY[10, 2], ARRAY[11, 12]
+);
+
+PREPARE randomOrder AS
+SELECT * FROM pgr_dagShortestPath(
+    'SELECT id, source, target, cost
+    FROM edge_table
+    ORDER BY RANDOM()',
+    ARRAY[10, 2], ARRAY[11, 12]
+);
+
+SELECT SETSEED(1);
+
+SELECT todo_start('Fix the code to return same set of rows');
+
+SELECT set_eq('expectedOutput', 'descendingOrder', '1: Should return same set of rows');
+SELECT set_eq('expectedOutput', 'randomOrder', '2: Should return same set of rows');
+SELECT set_eq('expectedOutput', 'randomOrder', '3: Should return same set of rows');
+SELECT set_eq('expectedOutput', 'randomOrder', '4: Should return same set of rows');
+SELECT set_eq('expectedOutput', 'randomOrder', '5: Should return same set of rows');
+
+SELECT todo_end();
+
+UPDATE edge_table SET cost = cost + 0.001 * id * id, reverse_cost = reverse_cost + 0.001 * id * id;
+
+SELECT set_eq('expectedOutput', 'descendingOrder', '6: Should return same set of rows');
+SELECT set_eq('expectedOutput', 'randomOrder', '7: Should return same set of rows');
+SELECT set_eq('expectedOutput', 'randomOrder', '8: Should return same set of rows');
+SELECT set_eq('expectedOutput', 'randomOrder', '9: Should return same set of rows');
+SELECT set_eq('expectedOutput', 'randomOrder', '10: Should return same set of rows');
+
+SELECT * FROM finish();
+ROLLBACK;
