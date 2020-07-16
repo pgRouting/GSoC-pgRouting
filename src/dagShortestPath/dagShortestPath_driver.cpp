fce696cd7d012f1d26b2c3c824c8085326e13255
Ashish Kumar <ashishkr23438@gmail.com>
2020-07-16 11:28:35 +0530

    [dagShortestPath] Fixed the code (sorted input rows)

--- src/dagShortestPath/dagShortestPath_driver.cpp
+++ src/dagShortestPath/dagShortestPath_driver.cpp
@@ -105,6 +105,12 @@ do_pgr_dagShortestPath(
 
         graphType gType = directed? DIRECTED: UNDIRECTED;
 
+        // sorting the edges in an ascending order of their id, before creating the graph
+        std::sort(data_edges, data_edges + total_edges,
+            [](const pgr_edge_t &edge1, const pgr_edge_t &edge2) -> bool {
+                return edge1.id < edge2.id;
+            });
+
         log << "Inserting vertices into a c++ vector structure";
         std::vector<int64_t>
             start_vertices(start_vidsArr, start_vidsArr + size_start_vidsArr);
