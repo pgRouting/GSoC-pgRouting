c6001c47a61cce8eb6674b78133195dd0984fb44
Ashish Kumar <ashishkr23438@gmail.com>
2020-07-16 11:00:47 +0530

    [breadthFirstSearch] Fixed the code (sorted input rows)

--- src/breadthFirstSearch/breadthFirstSearch_driver.cpp
+++ src/breadthFirstSearch/breadthFirstSearch_driver.cpp
@@ -87,6 +87,12 @@ do_pgr_breadthFirstSearch(
         std::vector<int64_t>
             start_vertices(start_vidsArr, start_vidsArr + size_start_vidsArr);
 
+        // sorting the edges in an ascending order of their id, before creating the graph
+        std::sort(data_edges, data_edges + total_edges,
+            [](const pgr_edge_t &edge1, const pgr_edge_t &edge2) -> bool {
+                return edge1.id < edge2.id;
+            });
+
         std::vector<pgr_mst_rt> results;
         if (directed) {
             log << "Working with directed Graph\n";
