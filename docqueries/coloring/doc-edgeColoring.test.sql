57533d5ae3a9b5441ce9655f34fcbaa2909d8713
veenits123 <123sveenit@gmail.com>
2021-06-17 08:50:16 +0530

    [docqueries][edgeColoring] modifying doc-edgeColoring.test.sql

--- docqueries/coloring/doc-edgeColoring.test.sql
+++ docqueries/coloring/doc-edgeColoring.test.sql
@@ -1,12 +1,14 @@
 \echo --q1
-SELECT * FROM pgr_bipartite(
-    $$SELECT id,source,target,cost,reverse_cost FROM edge_table$$
+SELECT * FROM pgr_edgeColoring(
+    'SELECT id, source, target, cost, reverse_cost FROM edge_table
+    ORDER BY id'
 );
 \echo --q2
 INSERT INTO edge_table (source, target, cost, reverse_cost) VALUES
 (1, 7, 1, 1);
 \echo --q3
-SELECT * FROM pgr_bipartite(
-    $$SELECT id,source,target,cost,reverse_cost FROM edge_table$$
+SELECT * FROM pgr_edgeColoring(
+    'SELECT id, source, target, cost, reverse_cost FROM edge_table
+    ORDER BY id'
 );
 \echo --q4
