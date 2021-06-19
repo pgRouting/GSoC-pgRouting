ddd8ca7e7e066d33eeddecf2b961989107d8ad62
veenits123 <123sveenit@gmail.com>
2021-06-19 21:22:58 +0530

    [docqueries][edgeColoring] fixing typo in doc-edgeColoring.result & doc-edgeColoring.test.sql

--- docqueries/coloring/doc-edgeColoring.test.sql
+++ docqueries/coloring/doc-edgeColoring.test.sql
@@ -1,14 +1,6 @@
-\echo --q1
+\echo -- q1
 SELECT * FROM pgr_edgeColoring(
     'SELECT id, source, target, cost, reverse_cost FROM edge_table
     ORDER BY id'
 );
-\echo --q2
-INSERT INTO edge_table (source, target, cost, reverse_cost) VALUES
-(1, 7, 1, 1);
-\echo --q3
-SELECT * FROM pgr_edgeColoring(
-    'SELECT id, source, target, cost, reverse_cost FROM edge_table
-    ORDER BY id'
-);
-\echo --q4
+\echo -- q2
