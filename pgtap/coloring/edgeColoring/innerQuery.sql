bc8383b78b89a9907a2c36872af8d9e82e571832
veenits123 <123sveenit@gmail.com>
2021-06-28 11:53:49 +0530

    [pgtap][edgeColoring] modifying innerQuery.sql

--- pgtap/coloring/edgeColoring/innerQuery.sql
+++ pgtap/coloring/edgeColoring/innerQuery.sql
@@ -3,7 +3,7 @@
 UPDATE edge_table SET cost = sign(cost), reverse_cost = sign(reverse_cost);
 SELECT plan(54);
 
-SELECT style_dijkstra('pgr_bipartite', ')');
+SELECT style_dijkstra('pgr_edgeColoring', ')');
 
-SELECT finish();
+SELECT * FROM finish();
 ROLLBACK;
