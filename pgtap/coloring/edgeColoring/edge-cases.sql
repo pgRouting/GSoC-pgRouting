2a86b3842f47a31d2d22a683a4deb4b1348a9156
veenits123 <123sveenit@gmail.com>
2021-06-30 23:12:08 +0530

    [pgtap][edgeColoring] updating edge-cases.sql

--- pgtap/coloring/edgeColoring/edge-cases.sql
+++ pgtap/coloring/edgeColoring/edge-cases.sql
@@ -1,7 +1,18 @@
 \i setup.sql
 
 UPDATE edge_table SET cost = sign(cost), reverse_cost = sign(reverse_cost);
-SELECT plan(16);
+SELECT CASE WHEN NOT min_version('3.3.0') THEN plan(1) ELSE plan(16) END;
+
+CREATE OR REPLACE FUNCTION edge_cases()
+RETURNS SETOF TEXT AS
+$BODY$
+BEGIN
+
+IF NOT min_version('3.3.0') THEN
+  RETURN QUERY
+  SELECT skip(1, 'Function is new on 3.3.0');
+  RETURN;
+END IF;
 
 -- 0 edge, 0 vertex test
 
@@ -10,6 +21,7 @@ SELECT id, source, target, cost, reverse_cost
 FROM edge_table
 WHERE id > 18;
 
+RETURN QUERY
 SELECT is_empty('q1', '1: Graph with 0 edge and 0 vertex');
 
 PREPARE edgeColoring1 AS
@@ -18,6 +30,7 @@ FROM pgr_edgeColoring(
     'q1'
 );
 
+RETURN QUERY
 SELECT is_empty('edgeColoring1', '2: Graph with 0 edge and 0 vertex -> Empty row is returned');
 
 
@@ -28,6 +41,7 @@ SELECT id, source, 2 AS target, cost, reverse_cost
 FROM edge_table
 WHERE id = 2;
 
+RETURN QUERY
 SELECT set_eq('q2', $$VALUES (2, 2, 2, -1, 1)$$, '3: Graph with only vertex 2');
 
 PREPARE edgeColoring2 AS
@@ -36,6 +50,7 @@ FROM pgr_edgeColoring(
     'q2'
 );
 
+RETURN QUERY
 SELECT is_empty('edgeColoring2', '4: One vertex graph can not be edgeColored-> Empty row is returned');
 
 
@@ -46,6 +61,7 @@ SELECT id, source, target, cost, reverse_cost
 FROM edge_table
 WHERE id = 7;
 
+RETURN QUERY
 SELECT set_eq('q3', $$VALUES (7, 8, 5, 1, 1)$$, '5: Graph with two connected vertices 8 and 5');
 
 PREPARE edgeColoring3 AS
@@ -54,7 +70,8 @@ FROM pgr_edgeColoring(
     'q3'
 );
 
-SELECT set_eq('edgeColoring3', $$VALUES (7, 2)$$, '6: Edge formed by connecting vertices 8 and 5, is colored with color 2');
+RETURN QUERY
+SELECT set_eq('edgeColoring3', $$VALUES (7, 1)$$, '6: Edge formed by connecting vertices 8 and 5, is colored with color 1');
 
 
 -- linear tests
@@ -66,6 +83,7 @@ SELECT id, source, target, cost, reverse_cost
 FROM edge_table
 WHERE id <= 2;
 
+RETURN QUERY
 SELECT set_eq('q4', $$VALUES (1, 1, 2, 1, 1), (2, 2, 3, -1, 1)$$, '7: Graph with three vertices 1, 2 and 3');
 
 PREPARE edgeColoring4 AS
@@ -74,8 +92,9 @@ FROM pgr_edgeColoring(
     'q4'
 );
 
-SELECT set_eq('edgeColoring4', $$VALUES (1, 3), (2, 2)$$,
-    '8: Edge (1, 2) is colored with color 3 and Edge (2, 3) is colored with color 2');
+RETURN QUERY
+SELECT set_eq('edgeColoring4', $$VALUES (1, 1), (2, 2)$$,
+    '8: Edge (1, 2) is colored with color 1 and Edge (2, 3) is colored with color 2');
 
 
 -- 4 vertices test
@@ -85,6 +104,7 @@ SELECT id, source, target, cost, reverse_cost
 FROM edge_table
 WHERE id <= 3;
 
+RETURN QUERY
 SELECT set_eq('q5',
     $$VALUES
         (1, 1, 2, 1, 1),
@@ -100,8 +120,9 @@ FROM pgr_edgeColoring(
     'q5'
 );
 
-SELECT set_eq('edgeColoring5', $$VALUES (1, 3), (2, 2), (3, 3)$$,
-    '10: Edge (1, 2) is colored with color 3, Edge (2, 3) is colored with color 2 and Edge (3, 4) is colored with color 3');
+RETURN QUERY
+SELECT set_eq('edgeColoring5', $$VALUES (1, 1), (2, 2), (3, 3)$$,
+    '10: Edge (1, 2) is colored with color 1, Edge (2, 3) is colored with color 2 and Edge (3, 4) is colored with color 3');
 
 
 -- even length cycle test
@@ -113,6 +134,7 @@ SELECT id, source, target, cost, reverse_cost
 FROM edge_table
 WHERE id IN (8, 10, 11, 12);
 
+RETURN QUERY
 SELECT set_eq('q6',
     $$VALUES
         (8, 5, 6, 1, 1),
@@ -129,8 +151,9 @@ FROM pgr_edgeColoring(
     'q6'
 );
 
-SELECT set_eq('edgeColoring6', $$VALUES (8, 1), (10, 5), (11, 5), (12, 3)$$,
-    '12: Edge (5, 6) is colored with color 1, Edge (5, 10) is colored with color 5, Edge (6, 11) is colored with color 5 and Edge (10, 11) is colored with color 3');
+RETURN QUERY
+SELECT set_eq('edgeColoring6', $$VALUES (8, 1), (10, 3), (11, 3), (12, 2)$$,
+    '12: Edge (5, 6) is colored with color 1, Edge (5, 10) is colored with color 3, Edge (6, 11) is colored with color 3 and Edge (10, 11) is colored with color 2');
 
 
 -- odd length cycle test
@@ -154,6 +177,7 @@ PREPARE q7 AS
 SELECT id, source, target, cost, reverse_cost
 FROM three_vertices_table;
 
+RETURN QUERY
 SELECT set_eq('q7',
     $$VALUES
         (1, 3, 6, 20, 15),
@@ -169,6 +193,7 @@ FROM pgr_edgeColoring(
     'q7'
 );
 
+RETURN QUERY
 SELECT set_eq('edgeColoring7', $$VALUES (1, 1), (2, 2), (3, 3)$$,
     '14: Edge (3, 6) is colored with color 1, Edge (3, 8) is colored with color 2 and Edge (6, 8) is colored with color 3');
 
@@ -194,6 +219,7 @@ PREPARE q8 AS
 SELECT id, source, target, cost, reverse_cost
 FROM five_vertices_table;
 
+RETURN QUERY
 SELECT set_eq('q8',
     $$VALUES
         (1, 1, 2, 1, 1),
@@ -211,8 +237,16 @@ FROM pgr_edgeColoring(
     'q8'
 );
 
-SELECT set_eq('edgeColoring8', $$VALUES (1, 1), (2, 2), (3, 1), (4, 2), (5, 3)$$,
-    '16: Edge (1, 2) is colored with color 1, Edge (2, 3) is colored with color 2, Edge (3, 4) is colored with color 1, Edge (4, 5) is colored with color 2 and Edge (5, 1) is colored with color 3');
+RETURN QUERY
+SELECT set_eq('edgeColoring8', $$VALUES (1, 1), (2, 2), (3, 3), (4, 1), (5, 2)$$,
+    '16: Edge (1, 2) is colored with color 1, Edge (2, 3) is colored with color 2, Edge (3, 4) is colored with color 3, Edge (4, 5) is colored with color 1 and Edge (5, 1) is colored with color 2');
+
+
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+SELECT edge_cases();
 
 
 SELECT * FROM finish();
