0dcf66f0b37be8273fcd7be2a0231ec169c20de8
veenits123 <123sveenit@gmail.com>
2021-06-30 16:36:03 +0530

    [pgtap][edgeColoring] modifying types_check.sql

--- pgtap/coloring/edgeColoring/types_check.sql
+++ pgtap/coloring/edgeColoring/types_check.sql
@@ -3,21 +3,21 @@
 UPDATE edge_table SET cost = sign(cost), reverse_cost = sign(reverse_cost);
 SELECT plan(5);
 
-SELECT has_function('pgr_bipartite');
+SELECT has_function('pgr_edgeColoring');
 
-SELECT has_function('pgr_bipartite', ARRAY['text']);
-SELECT function_returns('pgr_bipartite', ARRAY['text'],  'setof record');
+SELECT has_function('pgr_edgeColoring', ARRAY['text']);
+SELECT function_returns('pgr_edgeColoring', ARRAY['text'],  'setof record');
 
--- pgr_bipartite
+-- pgr_edgeColoring
 -- parameter names
 SELECT bag_has(
-    $$SELECT  proargnames from pg_proc where proname = 'pgr_bipartite'$$,
-    $$SELECT  '{"","vertex_id","color_id"}'::TEXT[] $$
+    $$SELECT  proargnames from pg_proc where proname = 'pgr_edgeColoring'$$,
+    $$SELECT  '{"","edge_id","color_id"}'::TEXT[] $$
 );
 
 -- parameter types
 SELECT set_eq(
-    $$SELECT  proallargtypes from pg_proc where proname = 'pgr_bipartite'$$,
+    $$SELECT  proallargtypes from pg_proc where proname = 'pgr_edgeColoring'$$,
     $$VALUES
         ('{25,20,20}'::OID[])
     $$
