e5ecc960ec226de668b0b0ecfbdee6f15e38f6fe
veenits123 <123sveenit@gmail.com>
2021-07-13 21:10:06 +0530

    [pgtap][edgeColoring] modifying edge-cases.sql

--- pgtap/coloring/edgeColoring/edge-cases.sql
+++ pgtap/coloring/edgeColoring/edge-cases.sql
@@ -22,13 +22,13 @@ FROM edge_table
 WHERE id > 18;
 
 RETURN QUERY
-SELECT is_empty('q1', '1: Graph with 0 edge and 0 vertex');
+SELECT is_empty('q1', 'Graph with 0 edge and 0 vertex');
 
 PREPARE edgeColoring1 AS
 SELECT * FROM pgr_edgeColoring('q1');
 
 RETURN QUERY
-SELECT is_empty('edgeColoring1', '2: Graph with 0 edge and 0 vertex -> Empty row is returned');
+SELECT is_empty('edgeColoring1', 'Graph with 0 edge and 0 vertex -> Empty row is returned');
 
 
 -- 1 vertex test
@@ -42,7 +42,7 @@ PREPARE edgeColoring2 AS
 SELECT * FROM pgr_edgeColoring('q2');
 
 RETURN QUERY
-SELECT is_empty('edgeColoring2', '4: One vertex graph can not be edgeColored-> Empty row is returned');
+SELECT is_empty('edgeColoring2', 'One vertex graph can not be edgeColored-> Empty row is returned');
 
 
 -- 2 vertices test (connected)
@@ -53,13 +53,13 @@ FROM edge_table
 WHERE id = 7;
 
 RETURN QUERY
-SELECT set_eq('q3', $$VALUES (7, 8, 5, 1, 1)$$, '5: Graph with two connected vertices 8 and 5');
+SELECT set_eq('q3', $$VALUES (7, 8, 5, 1, 1)$$, 'Graph with two connected vertices 8 and 5');
 
 PREPARE edgeColoring3 AS
 SELECT * FROM pgr_edgeColoring('q3');
 
 RETURN QUERY
-SELECT set_eq('edgeColoring3', $$VALUES (7, 1)$$, '6: Edge is colored with color 1');
+SELECT set_eq('edgeColoring3', $$VALUES (7, 1)$$, 'Edge is colored with color 1');
 
 
 -- linear tests
@@ -72,13 +72,13 @@ FROM edge_table
 WHERE id <= 2;
 
 RETURN QUERY
-SELECT set_eq('q4', $$VALUES (1, 1, 2, 1, 1), (2, 2, 3, -1, 1)$$, '7: Graph with three vertices 1, 2 and 3');
+SELECT set_eq('q4', $$VALUES (1, 1, 2, 1, 1), (2, 2, 3, -1, 1)$$, 'Graph with three vertices 1, 2 and 3');
 
 PREPARE edgeColoring4 AS
 SELECT * FROM pgr_edgeColoring('q4');
 
 RETURN QUERY
-SELECT set_eq('edgeColoring4', $$VALUES (1, 1), (2, 2)$$, '8: Two colors are required');
+SELECT set_eq('edgeColoring4', $$VALUES (1, 1), (2, 2)$$, 'Two colors are required');
 
 
 -- 4 vertices test
@@ -95,14 +95,14 @@ SELECT set_eq('q5',
         (2, 2, 3, -1, 1),
         (3, 3, 4, -1, 1)
     $$,
-    '9: Graph with four vertices 1, 2, 3 and 4'
+    'Graph with four vertices 1, 2, 3 and 4'
 );
 
 PREPARE edgeColoring5 AS
 SELECT * FROM pgr_edgeColoring('q5');
 
 RETURN QUERY
-SELECT set_eq('edgeColoring5', $$VALUES (1, 1), (2, 2), (3, 3)$$, '10: Three colors are required');
+SELECT set_eq('edgeColoring5', $$VALUES (1, 1), (2, 2), (3, 3)$$, 'Three colors are required');
 
 
 -- even length cycle test
@@ -122,14 +122,14 @@ SELECT set_eq('q6',
         (11, 6, 11, 1, -1),
         (12, 10, 11, 1, -1)
     $$,
-    '11: Graph with four vertices 5, 6, 10 and 11 (cyclic)'
+    'Graph with four vertices 5, 6, 10 and 11 (cyclic)'
 );
 
 PREPARE edgeColoring6 AS
 SELECT * FROM pgr_edgeColoring('q6');
 
 RETURN QUERY
-SELECT set_eq('edgeColoring6', $$VALUES (8, 1), (10, 3), (11, 3), (12, 2)$$, '12: Three colors are required');
+SELECT set_eq('edgeColoring6', $$VALUES (8, 1), (10, 3), (11, 3), (12, 2)$$, 'Three colors are required');
 
 
 -- odd length cycle test
@@ -160,14 +160,14 @@ SELECT set_eq('q7',
         (2, 3, 8, 10, -10),
         (3, 6, 8, -1, 12)
     $$,
-    '13: Cyclic Graph with three vertices 3, 6 and 8'
+    'Cyclic Graph with three vertices 3, 6 and 8'
 );
 
 PREPARE edgeColoring7 AS
 SELECT * FROM pgr_edgeColoring('q7');
 
 RETURN QUERY
-SELECT set_eq('edgeColoring7', $$VALUES (1, 1), (2, 2), (3, 3)$$, '14: Three colors are required');
+SELECT set_eq('edgeColoring7', $$VALUES (1, 1), (2, 2), (3, 3)$$, 'Three colors are required');
 
 
 -- 5 vertices cyclic
@@ -200,14 +200,14 @@ SELECT set_eq('q8',
         (4, 4, 5, 1, 1),
         (5, 5, 1, 1, -1);
     $$,
-    '15: Cyclic Graph with 5 vertices 3, 6 and 8'
+    'Cyclic Graph with 5 vertices 3, 6 and 8'
 );
 
 PREPARE edgeColoring8 AS
 SELECT * FROM pgr_edgeColoring('q8');
 
 RETURN QUERY
-SELECT set_eq('edgeColoring8', $$VALUES (1, 1), (2, 2), (3, 3), (4, 1), (5, 2)$$, '16: Three colors are required');
+SELECT set_eq('edgeColoring8', $$VALUES (1, 1), (2, 2), (3, 3), (4, 1), (5, 2)$$, 'Three colors are required');
 
 
 END;
