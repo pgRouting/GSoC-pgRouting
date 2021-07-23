2c1e0c3003a63f88e2582c588f98c5cc86d4c748
veenits123 <123sveenit@gmail.com>
2021-07-23 21:09:36 +0530

    [src][edgeColoring] refining pgr_edgeColoring.cpp

--- src/coloring/pgr_edgeColoring.cpp
+++ src/coloring/pgr_edgeColoring.cpp
@@ -36,6 +36,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 #include "cpp_common/identifiers.hpp"
 #include "cpp_common/pgr_assert.h"
+#include "cpp_common/interruption.h"
 
 
 namespace pgrouting {
@@ -45,9 +46,7 @@ std::vector<pgr_vertex_color_rt>
 Pgr_edgeColoring::edgeColoring() {
     std::vector<pgr_vertex_color_rt> results;
 
-#if 0
     CHECK_FOR_INTERRUPTS();
-#endif
 
     try {
         boost::edge_coloring(graph,  boost::get(boost::edge_bundle, graph));
@@ -98,11 +97,7 @@ Pgr_edgeColoring::Pgr_edgeColoring(pgr_edge_t *edges,
         if (edge.cost < 0 && edge.reverse_cost < 0) continue;
 
         E e;
-#if 1
         boost::tie(e, added) = boost::add_edge(v1, v2, edge.cost, graph);
-#else
-        boost::tie(e, added) = boost::add_edge(v1, v2, graph);
-#endif
 
         E_to_id.insert(std::make_pair(e, edge.id));
     }
@@ -113,8 +108,9 @@ Pgr_edgeColoring::get_boost_vertex(int64_t id) const {
     try {
         return id_to_V.at(id);
     } catch (...) {
-        pgassert(false);
-        throw;
+        throw std::make_pair(
+            std::string("INTERNAL: something went wrong when getting the vertex descriptor"),
+            std::string(__PRETTY_FUNCTION__));
     }
 }
 
@@ -123,8 +119,9 @@ Pgr_edgeColoring::get_vertex_id(V v) const {
     try {
         return V_to_id.at(v);
     } catch (...) {
-        pgassert(false);
-        throw;
+        throw std::make_pair(
+            std::string("INTERNAL: something went wrong when getting the vertex id"),
+            std::string(__PRETTY_FUNCTION__));
     }
 }
 
@@ -133,8 +130,9 @@ Pgr_edgeColoring::get_edge_id(E e) const {
     try {
         return E_to_id.at(e);
     } catch (...) {
-        pgassert(false);
-        throw;
+        throw std::make_pair(
+            std::string("INTERNAL: something went wrong when getting the edge id"),
+            std::string(__PRETTY_FUNCTION__));
     }
 }
 
