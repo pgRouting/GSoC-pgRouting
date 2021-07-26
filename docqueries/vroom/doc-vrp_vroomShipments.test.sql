2b3bbe7f6a3290aa8fb42005d40d65c1fda22359
Ashish Kumar <ashishkr23438@gmail.com>
2021-07-26 11:00:44 +0530

    [vroom][doc] Add docqueries for vroomShipments

--- docqueries/vroom/doc-vrp_vroomShipments.test.sql
+++ docqueries/vroom/doc-vrp_vroomShipments.test.sql
@@ -3,21 +3,28 @@ SELECT *
 FROM vrp_vroomShipments(
   $shipments$
     SELECT * FROM (
-      VALUES (100, 1, 200, 30)
+      VALUES (100, 1, 200, 4)
     ) AS C(p_id, p_location_index, d_id, d_location_index)
   $shipments$,
   $vehicles$
     SELECT * FROM (
-      VALUES (500, 1, 30)
+      VALUES (1, 1, 4)
     ) AS C(id, start_index, end_index)
   $vehicles$,
   $matrix$
     SELECT * FROM (
-      VALUES (1, 5, 2104), (1, 10, 197), (1, 30, 1299),
-             (5, 1, 2103), (5, 10, 2255), (5, 30, 3152),
-             (10, 1, 197), (10, 5, 2256), (10, 30, 1102),
-             (30, 1, 1299), (30, 5, 3153), (30, 10, 1102)
+      VALUES (1, 2, 2104), (1, 3, 197), (1, 4, 1299),
+             (2, 1, 2103), (2, 3, 2255), (2, 4, 3152),
+             (3, 1, 197), (3, 2, 2256), (3, 4, 1102),
+             (4, 1, 1299), (4, 2, 3153), (4, 3, 1102)
     ) AS C(start_vid, end_vid, agg_cost)
   $matrix$
 );
 \echo -- q2
+SELECT *
+FROM vrp_vroomShipments(
+  'SELECT * FROM vroom.shipments',
+  'SELECT * FROM vroom.vehicles',
+  'SELECT * FROM vroom.matrix'
+);
+\echo -- q3
