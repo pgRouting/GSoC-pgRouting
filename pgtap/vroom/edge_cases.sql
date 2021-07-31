b4ee12c6cd5aa89bf8bb4d703b2172b4dd3c5d2c
Ashish Kumar <ashishkr23438@gmail.com>
2021-07-31 21:10:33 +0530

    [vroom][pgtap] Added 5 edge cases

--- pgtap/vroom/edge_cases.sql
+++ pgtap/vroom/edge_cases.sql
@@ -1,7 +1,7 @@
 BEGIN;
 SET search_path TO 'vroom', 'public';
 
-SELECT CASE WHEN min_version('0.2.0') THEN plan (14) ELSE plan(1) END;
+SELECT CASE WHEN min_version('0.2.0') THEN plan (19) ELSE plan(1) END;
 
 CREATE OR REPLACE FUNCTION edge_cases()
 RETURNS SETOF TEXT AS
@@ -222,6 +222,113 @@ BEGIN
   RETURN QUERY
   SELECT set_eq('missing_reverse_cost_on_matrix', 'vroom_sql', 'Reverse cost is equal to the cost, if not specified');
 
+  -- one job/shipment tests
+
+  -- Column Names:
+  -- seq, vehicle_seq, vehicle_id, step_seq, step_type, task_id,
+  -- arrival, travel_time, service_time, waiting_time, load
+
+  PREPARE one_job_q1 AS
+  SELECT * FROM vrp_vroom(
+    'SELECT * FROM jobs WHERE id = 1',
+    'empty_shipments',
+    'vehicles',
+    'matrix'
+  );
+  RETURN QUERY
+  SELECT set_eq('one_job_q1',
+    $$
+      VALUES
+      (1, 1, 1, 1, 1, -1, 300, 0, 0, 0, ARRAY[20]),
+      (2, 1, 1, 2, 5, -1, 300, 0, 0, 0, ARRAY[20]),
+      (3, 1, 1, 3, 2, 1, 300, 0, 250, 3325, ARRAY[20]),
+      (4, 1, 1, 4, 6, -1, 3875, 0, 0, 0, ARRAY[20])
+    $$,
+    'Problem with only one job having id 1'
+  );
+
+  PREPARE one_job_q2 AS
+  SELECT * FROM vrp_vroom(
+    'SELECT * FROM jobs WHERE id = 5',
+    'empty_shipments',
+    'vehicles',
+    'matrix'
+  );
+  RETURN QUERY
+  SELECT set_eq('one_job_q2',
+    $$
+      VALUES
+      (1, 1, 4, 1, 1, -1, 250, 0, 0, 0, ARRAY[20]),
+      (2, 1, 4, 2, 5, -1, 250, 0, 0, 0, ARRAY[20]),
+      (3, 1, 4, 3, 2, 5, 300, 50, 250, 725, ARRAY[20]),
+      (4, 1, 4, 4, 6, -1, 1325, 100, 0, 0, ARRAY[20])
+    $$,
+    'Problem with only one job having id 5'
+  );
+
+  PREPARE one_shipment_q1 AS
+  SELECT * FROM vrp_vroom(
+    'empty_jobs',
+    'SELECT * FROM shipments WHERE id = 1',
+    'vehicles',
+    'matrix'
+  );
+  RETURN QUERY
+  SELECT set_eq('one_shipment_q1',
+    $$
+      VALUES
+      (1, 1, 4, 1, 1, -1, 250, 0, 0, 0, ARRAY[0]),
+      (2, 1, 4, 2, 5, -1, 250, 0, 0, 0, ARRAY[0]),
+      (3, 1, 4, 3, 3, 6, 250, 0, 2250, 1375, ARRAY[10]),
+      (4, 1, 4, 4, 4, 7, 3900, 25, 2250, 21025, ARRAY[0]),
+      (5, 1, 4, 5, 6, -1, 27200, 50, 0, 0, ARRAY[0])
+    $$,
+    'Problem with only one shipment having id 1'
+  );
+
+  PREPARE one_shipment_q2 AS
+  SELECT * FROM vrp_vroom(
+    'empty_jobs',
+    'SELECT * FROM shipments WHERE id = 5',
+    'vehicles',
+    'matrix'
+  );
+  RETURN QUERY
+  SELECT set_eq('one_shipment_q2',
+    $$
+      VALUES
+      (1, 1, 1, 1, 1, -1, 300, 0, 0, 0, ARRAY[0]),
+      (2, 1, 1, 2, 5, -1, 300, 0, 0, 0, ARRAY[0]),
+      (3, 1, 1, 3, 3, 14, 350, 50, 2250, 13000, ARRAY[10]),
+      (4, 1, 1, 4, 4, 15, 15600, 50, 2250, 2575, ARRAY[0]),
+      (5, 1, 1, 5, 6, -1, 20475, 100, 0, 0, ARRAY[0])
+    $$,
+    'Problem with only one shipment having id 5'
+  );
+
+  PREPARE one_job_shipment AS
+  SELECT * FROM vrp_vroom(
+    'SELECT * FROM jobs WHERE id = 2',
+    'SELECT * FROM shipments WHERE id = 2',
+    'vehicles',
+    'matrix'
+  );
+  RETURN QUERY
+  SELECT set_eq('one_job_shipment',
+    $$
+      VALUES
+      (1, 1, 1, 1, 1, -1, 300, 0, 0, 0, ARRAY[30]),
+      (2, 1, 1, 2, 5, -1, 300, 0, 0, 0, ARRAY[30]),
+      (3, 1, 1, 3, 2, 2, 350, 50, 250, 900, ARRAY[30]),
+      (4, 1, 1, 4, 6, -1, 1550, 100, 0, 0, ARRAY[30]),
+      (5, 2, 4, 1, 1, -1, 250, 0, 0, 0, ARRAY[0]),
+      (6, 2, 4, 2, 5, -1, 250, 0, 0, 0, ARRAY[0]),
+      (7, 2, 4, 3, 3, 8, 275, 25, 2250, 100, ARRAY[10]),
+      (8, 2, 4, 4, 4, 9, 2736, 136, 2250, 1514, ARRAY[0]),
+      (9, 2, 4, 5, 6, -1, 6590, 226, 0, 0, ARRAY[0])
+    $$,
+    'Problem with one job and one shipment having id 2'
+  );
 
 END;
 $BODY$
