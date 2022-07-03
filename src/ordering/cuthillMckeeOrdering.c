36d8789d37b7a66071bb37851fcf5b8166c03545
Shobhit <000shobhitchaurasia@gmail.com>
2022-07-03 23:43:15 +0530

    [cuthillMckeeOrdering][src] Fixing compilatio error of cuthillMckeeOrdering.c

--- src/ordering/cuthillMckeeOrdering.c
+++ src/ordering/cuthillMckeeOrdering.c
@@ -25,10 +25,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
  ********************************************************************PGR-GNU*/
 
-/** @file cuthillMckeeOrdering.c
- * @brief Connecting code with postgres.
- *
- */
 #include <stdbool.h>
 #include "c_common/postgres_connection.h"
 #include "utils/array.h"
@@ -44,28 +40,14 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 #include "drivers/ordering/cuthillMckeeOrdering_driver.h"
 
-PGDLLEXPORT Datum _cuthillMckeeOrdering(PG_FUNCTION_ARGS);
-PG_FUNCTION_INFO_V1(_cuthillMckeeOrdering);
-
-/** @brief Static function, loads the data from postgres to C types for further processing.
- *
- * It first connects the C function to the SPI manager. Then converts
- * the postgres array to C array and loads the edges belonging to the graph
- * in C types. Then it calls the function `do_cuthillMckeeOrdering` defined
- * in the `cuthillMckeeOrdering_driver.h` file for further processing.
- * Finally, it frees the memory and disconnects the C function to the SPI manager.
- *
- * @param edges_sql      the edges of the SQL query
- * @param result_tuples  the rows in the result
- * @param result_count   the count of rows in the result
- *
- * @returns void
- */
+PGDLLEXPORT Datum _pgr_cuthillmckeeordering(PG_FUNCTION_ARGS);
+PG_FUNCTION_INFO_V1(_pgr_cuthillmckeeordering);
 
 static
 void
 process(
     char* edges_sql,
+    int64_t start_vid,
 
     II_t_rt **result_tuples,
     size_t *result_count) {
@@ -74,13 +56,13 @@ process(
     (*result_tuples) = NULL;
     (*result_count) = 0;
 
-    pgr_edge_t *edges = NULL;
+    Edge_t *edges = NULL;
     size_t total_edges = 0;
 
     pgr_get_edges(edges_sql, &edges, &total_edges);
     if (total_edges == 0) {
         ereport(WARNING,
-                (errmsg("Insufficient data found on inner query."),
+                (errmsg("Insufficient data edges SQL."),
                  errhint("%s", edges_sql)));
         (*result_count) = 0;
         (*result_tuples) = NULL;
@@ -102,7 +84,7 @@ process(
             &notice_msg,
             &err_msg);
 
-    time_msg("processing _cuthillMckeeOrdering", start_t, clock());
+    time_msg("processing cuthillmckeeordering", start_t, clock());
 
     if (err_msg && (*result_tuples)) {
         pfree(*result_tuples);
@@ -120,39 +102,28 @@ process(
     pgr_SPI_finish();
 }
 
-/*                                                                            */
-/******************************************************************************/
-
 /** @brief Helps in converting postgres variables to C variables, and returns the result.
  *
  */
 
-PGDLLEXPORT Datum _cuthillMckeeOrdering(PG_FUNCTION_ARGS) {
+PGDLLEXPORT Datum 
+_pgr_cuthillmckeeordering(PG_FUNCTION_ARGS) {
     FuncCallContext     *funcctx;
     TupleDesc           tuple_desc;
 
-    /**********************************************************************/
     II_t_rt *result_tuples = NULL;
     size_t result_count = 0;
-    /**********************************************************************/
-
+  
     if (SRF_IS_FIRSTCALL()) {
         MemoryContext   oldcontext;
         funcctx = SRF_FIRSTCALL_INIT();
         oldcontext = MemoryContextSwitchTo(funcctx->multi_call_memory_ctx);
 
-        /***********************************************************************
-         *
-         *   _curthillMckeeOrdering(edges_sql TEXT);
-         *
-         **********************************************************************/
-
         process(
                 text_to_cstring(PG_GETARG_TEXT_P(0)),
+                PG_GETARG_INT64(1),
                 &result_tuples,
                 &result_count);
-
-        /**********************************************************************/
 
     funcctx->max_calls = result_count;
 
@@ -190,10 +161,10 @@ PGDLLEXPORT Datum _cuthillMckeeOrdering(PG_FUNCTION_ARGS) {
             nulls[i] = false;
         }
 
-        // values[0] = Int64GetDatum(result_tuples[funcctx->call_cntr].d1.id);
-        // values[1] = Int64GetDatum(result_tuples[funcctx->call_cntr].d2.value);
-
-        /**********************************************************************/
+        #if 0
+        values[0] = Int64GetDatum(result_tuples[funcctx->call_cntr].d1.id);
+        values[1] = Int64GetDatum(result_tuples[funcctx->call_cntr].d2.value);
+        #endif 
 
         tuple = heap_form_tuple(tuple_desc, values, nulls);
         result = HeapTupleGetDatum(tuple);
