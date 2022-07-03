8d28e6c197764db321957ed100be773b19c48804
Manas <manas23601@gmail.com>
2022-07-03 18:17:55 +0530

    [src] added template code to knapsack.c and knapsack.driver.cpp

--- include/drivers/or_tools/knapsack_driver.h
+++ include/drivers/or_tools/knapsack_driver.h
@@ -28,8 +28,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 /*! @file knapsack_driver.h */
 
-#ifndef INCLUDE_DRIVERS_PGR_PICKDELIVER_PICKDELIVER_DRIVER_H_
-#define INCLUDE_DRIVERS_PGR_PICKDELIVER_PICKDELIVER_DRIVER_H_
+#ifndef INCLUDE_DRIVERS_OR_TOOLS_KNAPSACK_DRIVER_H_
+#define INCLUDE_DRIVERS_OR_TOOLS_KNAPSACK_DRIVER_H_
 #pragma once
 
 /* for size-t */
