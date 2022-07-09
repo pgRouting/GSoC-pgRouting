79292d1c6315dd53ee6cb83a8c9ceb70dab65045
ntishchauhan0022 <nitishchauhan0022@gmail.com>
2022-07-09 13:34:13 +0530

    [pgr_hawickcircuits] fixing typo in header file include

--- src/circuits/hawickCircuits.c
+++ src/circuits/hawickCircuits.c
@@ -43,7 +43,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include "c_common/arrays_input.h"
 
 
-#include "drivers/circuits/hawickCircuits_driver.h"
+#include "drivers/circuits/hawickcircuits_driver.h"
 
 PGDLLEXPORT Datum _pgr_hawickCircuits(PG_FUNCTION_ARGS);
 PG_FUNCTION_INFO_V1(_pgr_hawickCircuits);
