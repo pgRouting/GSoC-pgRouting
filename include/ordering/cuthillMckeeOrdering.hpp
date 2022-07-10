4dc9836aafb7d28a814b931c92f7f1ee7193bf11
Shobhit <000shobhitchaurasia@gmail.com>
2022-07-10 23:43:00 +0530

    [cuthillMckeeOrdering][include] changes for running boost example

--- include/ordering/cuthillMckeeOrdering.hpp
+++ include/ordering/cuthillMckeeOrdering.hpp
@@ -31,12 +31,13 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #define INCLUDE_ORDERING_CUTHILLMCKEEORDERING_HPP_
 #pragma once
 
+/* TODO remove unnecessary includes */
 #include <boost/property_map/property_map.hpp>
 #include <boost/graph/graph_traits.hpp>
 #include <boost/property_map/vector_property_map.hpp>
 #include <boost/type_traits.hpp>
 #include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/sequential_vertex_coloring.hpp>
+#include <boost/graph/cuthill_mckee_ordering.hpp>
 
 #include <algorithm>
 #include <vector>
@@ -44,6 +45,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 #include "cpp_common/pgr_base_graph.hpp"
 #include "cpp_common/interruption.h"
+#include "cpp_common/pgr_messages.h"
 
 #include "c_types/ii_t_rt.h"
 
@@ -60,13 +62,10 @@ namespace functions {
 
 //*************************************************************
 
-template <class G>
-class CuthillMckeeOrdering {
- public:
-     using V = typename G::V;
-     using E = typename G::E;
-     using Graph = boost::adjacency_list<boost::listS, boost::vecS, boost::undirectedS>;
-     using vertices_size_type = boost::graph_traits<Graph>::vertices_size_type;
+class CuthillMckeeOrdering : public Pgr_messages{
+public:
+    using G = pgrouting::UndirectedGraph;
+    using vertices_size_type = G::vertices_size_type;
 
      /** @name cuthillMckeeOrdering
       * @{
@@ -84,10 +83,17 @@ class CuthillMckeeOrdering {
       * @see [boost::cuthill_mckee_ordering]
       * (https://www.boost.org/libs/graph/doc/cuthill_mckee_ordering.html)
       */
-     std::vector <II_t_rt> cuthillMckeeOrdering(G &graph) {
-         std::vector <II_t_rt> results;
+    std::vector<II_t_rt>cuthillMckeeOrdering(G & /*graph*/, int64_t) {
+        std::vector<II_t_rt>results;
 
-        #if 0
+        /* TODO
+         * start_vid -> vertex descriptor
+         * make a call
+         */
+        
+        /* On the meantime, using boost example */
+
+#if 0
          auto i_map = boost::get(boost::vertex_index, graph.graph);
 
          // vector which will store the ordering of the graph
@@ -112,8 +118,66 @@ class CuthillMckeeOrdering {
          }
 
          results = get_results(ordering, graph);
-        #endif
+#else
+    {
+        using namespace boost;
+        typedef adjacency_list< vecS, vecS, undirectedS,
+        property< vertex_color_t, default_color_type,
+        property< vertex_degree_t, int64_t > > >
+        Graph;
+        typedef graph_traits< Graph >::vertex_descriptor Vertex;
+        typedef graph_traits< Graph >::vertices_size_type size_type;
 
+        typedef std::pair< std::int64_t, std::int64_t > Pair;
+        Pair edges[14] = { Pair(0, 3), // a-d
+            Pair(0, 5), // a-f
+            Pair(1, 2), // b-c
+            Pair(1, 4), // b-e
+            Pair(1, 6), // b-g
+            Pair(1, 9), // b-j
+            Pair(2, 3), // c-d
+            Pair(2, 4), // c-e
+            Pair(3, 5), // d-f
+            Pair(3, 8), // d-i
+            Pair(4, 6), // e-g
+            Pair(5, 6), // f-g
+            Pair(5, 7), // f-h
+            Pair(6, 7) }; // g-h
+
+        Graph G(10);
+            for (int i = 0; i < 14; ++i)
+            add_edge(edges[i].first, edges[i].second, G);
+
+        graph_traits< Graph >::vertex_iterator ui, ui_end;
+
+        property_map< Graph, vertex_degree_t >::type deg = get(vertex_degree, G);
+            for (boost::tie(ui, ui_end) = vertices(G); ui != ui_end; ++ui)
+            deg[*ui] = degree(*ui, G);
+
+        property_map< Graph, vertex_index_t >::type index_map
+            = get(vertex_index, G);
+        
+        std::vector< Vertex > inv_perm(num_vertices(G));
+        std::vector< size_type > perm(num_vertices(G));
+    {
+        Vertex s = vertex(6, G);
+        // reverse cuthill_mckee_ordering
+        cuthill_mckee_ordering(G, s, inv_perm.rbegin(), get(vertex_color, G),
+            get(vertex_degree, G));
+        log << "Reverse Cuthill-McKee ordering starting at: " << s << std::endl;
+        log << "  ";
+        for (std::vector< Vertex >::const_iterator i = inv_perm.begin();
+             i != inv_perm.end(); ++i) {
+            log << index_map[*i] << " ";
+            results.push_back({index_map[*i],index_map[*i]});
+            }
+        log << std::endl;
+
+        for (size_type c = 0; c != inv_perm.size(); ++c)
+            perm[index_map[inv_perm[c]]] = c;
+        }
+    }        
+#endif
          return results;
      }
 
@@ -130,10 +194,12 @@ class CuthillMckeeOrdering {
       * @returns `results` vector
       */
      std::vector <II_t_rt> get_results(
-             std::vector < vertices_size_type > &ordering,
-             const G &graph) {
+             std::vector <vertices_size_type> & /*ordering*/,
+             const G & /*graph*/) {
+        
          std::vector <II_t_rt> results;
-
+          
+#if 0 
          typename boost::graph_traits < Graph > ::vertex_iterator v, vend;
 
          for (boost::tie(v, vend) = vertices(graph.graph); v != vend; ++v) {
@@ -143,12 +209,11 @@ class CuthillMckeeOrdering {
          }
 
          // ordering the results in an reverse ordering 
-         #if 0
          std::sort(results.begin(), results.end(),
              [](const II_t_rt row1, const II_t_rt row2) {
                  return row1.d1.id < row2.d1.id;
              });
-         #endif    
+#endif    
 
          return results;
      }
