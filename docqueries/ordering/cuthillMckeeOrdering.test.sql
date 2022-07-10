f35e6e3c0c63f8ca03149ce3d627219035c1931f
Shobhit <000shobhitchaurasia@gmail.com>
2022-07-10 23:38:49 +0530

    [cuthillMckeeOrdering][docqueries] adding boost example query

--- docqueries/ordering/cuthillMckeeOrdering.test.sql
+++ docqueries/ordering/cuthillMckeeOrdering.test.sql
@@ -1,6 +1,25 @@
 /* -- q1 */
 SELECT * FROM pgr_cuthillMckeeOrdering(
-    'SELECT id, source, target, cost, reverse_cost FROM edges',
-    1
-);
+  'SELECT id, source, target, cost, reverse_cost FROM edges',
+  1);
 /* -- q2 */
+-- below this line are for becoming pgTap tests
+SELECT * FROM pgr_cuthillMckeeOrdering(
+  'SELECT id, source, target, cost, reverse_cost FROM edges',
+  6);
+
+CREATE TABLE expected_result (
+  seq INTEGER,
+  ordering BIGINT);
+
+INSERT INTO expected_result (seq, ordering) VALUES
+(1, 8),
+(2, 3),
+(3, 0),   
+(4, 9),
+(5, 2),
+(6, 5),
+(7, 1),
+(8, 4),
+(9, 7),
+(10, 6);
