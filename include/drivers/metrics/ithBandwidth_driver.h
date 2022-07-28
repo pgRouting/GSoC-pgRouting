ccb2f240771c0b85e24647e5debc7ebe7fb06272
Sanskar Bhushan <sbdtu5498@gmail.com>
2022-07-28 17:46:59 +0530

    [include][drivers][metrics] modified ithBandwidth_driver.h

--- include/drivers/metrics/ithBandwidth_driver.h
+++ include/drivers/metrics/ithBandwidth_driver.h
@@ -21,8 +21,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 ********************************************************************PGR-GNU*/
 
-#ifndef INCLUDE_DRIVERS_COLORING_ITHBANDWIDTH_DRIVER_H_
-#define INCLUDE_DRIVERS_COLORING_ITHBANDWIDTH_DRIVER_H_
+#ifndef INCLUDE_DRIVERS_METRICS_ITHBANDWIDTH_DRIVER_H_
+#define INCLUDE_DRIVERS_METRICS_ITHBANDWIDTH_DRIVER_H_
 #pragma once
 
 /* for size-t */
@@ -63,5 +63,5 @@ extern "C" {
 }
 #endif
 
-#endif  // INCLUDE_DRIVERS_COLORING_ITHBANDWIDTH_DRIVER_H_
+#endif  // INCLUDE_DRIVERS_METRICS_ITHBANDWIDTH_DRIVER_H_
 
