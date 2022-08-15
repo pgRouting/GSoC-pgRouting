1679240186a7eef60810820e83c08c9f80200b06
Manas <manas23601@gmail.com>
2022-08-16 01:29:45 +0530

    [pgtap] not working

--- pgtap/or_tools/no_crash_test.sql
+++ pgtap/or_tools/no_crash_test.sql
@@ -1,27 +1,7 @@
 BEGIN;
 SET search_path TO 'ortools', 'public';
 SET client_min_messages TO ERROR;
-
-SELECT CASE WHEN min_version('0.3.0') THEN plan (116) ELSE plan(1) END;
-
--- DEALLOCATE knapsack_test;
--- DEALLOCATE multiple_knapsack_test;
--- DEALLOCATE bin_packing_test;
-
--- PREPARE knapsack_test AS
--- SELECT * FROM ortools.knapsack_data;
-
--- PREPARE multiple_knapsack_test AS
--- SELECT * FROM ortools.multiple_knapsack_data;
-
--- PREPARE bin_packing_test AS
--- SELECT * FROM ortools.bin_packing_data;
-
-
--- SELECT isnt_empty('knapsack_test', 'Should be not empty to tests be meaningful');
--- SELECT isnt_empty('multiple_test', 'Should be not empty to tests be meaningful');
--- SELECT isnt_empty('bin_packing_test', 'Should be not empty to tests be meaningful');
-
+SELECT plan(13);
 
 CREATE OR REPLACE FUNCTION no_crash()
 RETURNS SETOF TEXT AS
@@ -29,7 +9,24 @@ $BODY$
 DECLARE
 params TEXT[];
 subs TEXT[];
+error_messages TEXT[];
+non_empty_args INTEGER[];
 BEGIN
+    -- DEALLOCATE knapsack_test;
+    -- DEALLOCATE multiple_knapsack_test;
+    -- DEALLOCATE bin_packing_test;
+
+    -- PREPARE knapsack_test AS SELECT * FROM ortools.knapsack_data;
+    -- PREPARE multiple_knapsack_test AS SELECT * FROM ortools.multiple_knapsack_data;
+    -- PREPARE bin_packing_test AS SELECT * FROM ortools.bin_packing_data;
+    
+    -- RETURN QUERY
+    -- SELECT isnt_empty('knapsack_test', 'Should be not empty to tests be meaningful');
+    -- RETURN QUERY
+    -- SELECT isnt_empty('multiple_test', 'Should be not empty to tests be meaningful');
+    -- RETURN QUERY
+    -- SELECT isnt_empty('bin_packing_test', 'Should be not empty to tests be meaningful');
+
     -- vrp_knapsack
     params = ARRAY[
     '$$SELECT * FROM ortools.knapsack_data$$',
@@ -38,20 +35,27 @@ BEGIN
     ]::TEXT[];
     subs = ARRAY[
     'NULL',
+    'capacity => 15',
     'NULL'
     ]::TEXT[];
+    error_messages = ARRAY[
+    '',
+    '',
+    ''
+    ]::TEXT[];
+    non_empty_args = ARRAY[0, 1, 2]::INTEGER[];
     
-    RETURN QUERY SELECT * FROM no_crash_test('vrp_knapsack', params, subs);
+    RETURN QUERY SELECT * FROM no_crash_test('vrp_knapsack', params, subs, error_messages, non_empty_args);
 
     --vrp_multiple_knapsack
     params = ARRAY[
-    '$$SELECT * FROM ortools.multiple_knapsack_data$$',
+    '$$multiple_knapsack_test$$',
     'capacities => ARRAY[100,100,100,100,100]',
     'max_rows => 100000'
     ]::TEXT[];
     subs = ARRAY[
     'NULL',
-    'NULL',
+    'capacities => ARRAY[100,100,100,100,100]',
     'NULL'
     ]::TEXT[];
     
@@ -59,13 +63,13 @@ BEGIN
 
     -- bin_packing
     params = ARRAY[
-    '$$SELECT * FROM ortools.bin_packing_data$$',
+    '$$bin_packing_test$$',
     'bin_capacity => 100'
     'max_rows => 100000'
     ]::TEXT[];
     subs = ARRAY[
     'NULL',
-    'NULL',
+    'bin_capacity => 100',
     'NULL'
     ]::TEXT[];
     
@@ -73,7 +77,6 @@ BEGIN
 END
 $BODY$
 LANGUAGE plpgsql VOLATILE;
-
 
 SELECT * FROM no_crash();
 
