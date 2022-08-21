73b3ed5d7c81d093e736c01d39b3fb2bbebef023
Manas <manas23601@gmail.com>
2022-08-21 16:25:12 +0530

    removed an unnecessary file

--- sql/or_tools/plpython/envir.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE FUNCTION blah(varchar) RETURNS integer AS $$
-    import sys
-    import ortools
-    plpy.notice("python exec = '%s'" % sys.executable)
-    plpy.notice("python version = '%s'" % sys.version)
-    plpy.notice("python path = '%s'" % sys.path)
-    return 1
-$$ LANGUAGE plpython3u;
