35e557a741662f2c62d7ba9593d3c4f06956ffdf
Shobhit <000shobhitchaurasia@gmail.com>
2022-08-21 16:43:15 +0530

    [docqueries] single query for now

--- docqueries/ordering/cuthillMckeeOrdering.test.sql
+++ docqueries/ordering/cuthillMckeeOrdering.test.sql
@@ -3,23 +3,3 @@ SELECT * FROM pgr_cuthillMckeeOrdering(
   'SELECT id, source, target, cost, reverse_cost FROM edges',
   1);
 /* -- q2 */
--- below this line are for becoming pgTap tests
-SELECT * FROM pgr_cuthillMckeeOrdering(
-  'SELECT id, source, target, cost, reverse_cost FROM edges',
-  6);
-
-CREATE TABLE expected_result (
-  seq INTEGER,
-  node BIGINT);
-
-INSERT INTO expected_result (seq, node) VALUES
-(1, 8),
-(2, 3),
-(3, 0), 
-(4, 9),
-(5, 2),
-(6, 5),
-(7, 1),
-(8, 4),
-(9, 7),
-(10, 6);
