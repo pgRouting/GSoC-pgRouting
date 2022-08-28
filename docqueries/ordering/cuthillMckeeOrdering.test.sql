14d773edf570f49b0078e48aaed94456efaedfa2
Shobhit <000shobhitchaurasia@gmail.com>
2022-08-29 00:10:44 +0530

    [docqueries] removing starting vertex

--- docqueries/ordering/cuthillMckeeOrdering.test.sql
+++ docqueries/ordering/cuthillMckeeOrdering.test.sql
@@ -1,35 +1,33 @@
 /* -- q1 */
 SELECT * FROM pgr_cuthillMckeeOrdering(
-  'SELECT id, source, target, cost, reverse_cost FROM edges',
-  1);
+  'SELECT id, source, target, cost, reverse_cost FROM edges'
+  );
 /* -- q2 */
 
 
 
 -- added to pgtap test
-SELECT * FROM pgr_cuthillMckeeOrdering(
-  'SELECT id, source, target, cost, reverse_cost FROM edges',
-  6);
+
 
 CREATE TABLE expected_result (
-  seq INTEGER,
+  seq BIGINT,
   node BIGINT);
 
 INSERT INTO expected_result (seq, node) VALUES
 (1, 13),
 (2, 14),
-(3, 9), 
-(4, 2),
-(5, 1),
-(6, 4),
-(7, 3),
-(8, 8),
-(9, 5),
+(3, 2),
+(4, 4),
+(5, 9),
+(6, 1),
+(7, 8),
+(8, 3),
+(9, 12),
 (10, 7),
-(11, 12),
-(12, 6),
+(11, 5),
+(12, 17),
 (13, 11),
-(14, 17),
-(15, 10),
-(16, 16),
+(14, 6),
+(15, 16),
+(16, 10),
 (17, 15);
