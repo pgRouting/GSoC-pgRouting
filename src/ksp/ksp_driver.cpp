960c67d8aae7184f134d6c21d4de9163e52cc759
Aniket-debug <aniketgarg187@gmail.com>
2023-06-06 20:18:57 +0530

    added one-to-many overload and try to run pgtap on ksp

--- src/ksp/ksp_driver.cpp
+++ src/ksp/ksp_driver.cpp
@@ -129,3 +129,113 @@ void  do_pgr_ksp(
         *log_msg = pgr_msg(log.str().c_str());
     }
 }
+
+
+/*
+*
+* creating one to many function
+*
+*/
+
+
+void do_pgr_ksp_one_to_many(
+        Edge_t *data_edges,
+        size_t total_edges,
+        int64_t start_vid,
+        int64_t *end_vids,
+        size_t end_vid_count,
+        size_t k,
+        bool directed,
+        bool heap_paths,
+        Path_rt **return_tuples,
+        size_t *return_count,
+        char **log_msg,
+        char **notice_msg,
+        char **err_msg) {
+    using pgrouting::Path;
+    using pgrouting::pgr_alloc;
+    using pgrouting::pgr_msg;
+    using pgrouting::pgr_free;
+
+    std::ostringstream err;
+    std::ostringstream log;
+    std::ostringstream notice;
+    try {
+        pgassert(!(*log_msg));
+        pgassert(!(*notice_msg));
+        pgassert(!(*err_msg));
+        pgassert(!(*return_tuples));
+        pgassert(*return_count == 0);
+        pgassert(total_edges != 0);
+        
+        graphType gType = directed ? DIRECTED : UNDIRECTED;
+        std::deque<Path> paths;
+        
+        if (directed) {
+            pgrouting::DirectedGraph digraph(gType);
+            Pgr_ksp<pgrouting::DirectedGraph> fn_yen;
+            digraph.insert_edges(data_edges, total_edges);
+            
+            for (size_t i = 0; i < end_vid_count; ++i) {
+                paths = fn_yen.Yen(digraph, start_vid, end_vids[i], k, heap_paths);
+                
+                auto count = count_tuples(paths);
+                if (count != 0) {
+                    *return_tuples = pgr_alloc(count, (*return_tuples));
+                    
+                    size_t sequence = 0;
+                    int route_id = 0;
+                    for (const auto &path : paths) {
+                        if (path.size() > 0)
+                            path.get_pg_ksp_path(return_tuples, sequence, route_id);
+                        ++route_id;
+                    }
+                    *return_count += count;
+                }
+            }
+        } else {
+            pgrouting::UndirectedGraph undigraph(gType);
+            Pgr_ksp<pgrouting::UndirectedGraph> fn_yen;
+            undigraph.insert_edges(data_edges, total_edges);
+            
+            for (size_t i = 0; i < end_vid_count; ++i) {
+                paths = fn_yen.Yen(undigraph, start_vid, end_vids[i], k, heap_paths);
+                
+                auto count = count_tuples(paths);
+                if (count != 0) {
+                    *return_tuples = pgr_alloc(count, (*return_tuples));
+                    
+                    size_t sequence = 0;
+                    int route_id = 0;
+                    for (const auto &path : paths) {
+                        if (path.size() > 0)
+                            path.get_pg_ksp_path(return_tuples, sequence, route_id);
+                        ++route_id;
+                    }
+                    *return_count += count;
+                }
+            }
+        }
+
+        pgassert(*err_msg == NULL);
+        *log_msg = log.str().empty() ?
+            *log_msg :
+            pgr_msg(log.str().c_str());
+        *notice_msg = notice.str().empty() ?
+            *notice_msg :
+            pgr_msg(notice.str().c_str());
+    } catch (std::exception &except) {
+        (*return_tuples) = pgr_free(*return_tuples);
+        (*return_count) = 0;
+        err << except.what();
+        *err_msg = pgr_msg(err.str().c_str());
+        *log_msg = pgr_msg(log.str().c_str());
+    } catch(...) {
+        (*return_tuples) = pgr_free(*return_tuples);
+        (*return_count) = 0;
+        err << "Caught unknown exception!";
+        *err_msg = pgr_msg(err.str().c_str());
+        *log_msg = pgr_msg(log.str().c_str());
+    }
+}
+
