9e44760f4a1144f8a8f53be5a9b6f4a0d6286291
Squarege <1263174378@qq.com>
2023-06-15 17:42:16 +0800

    [withPointsDD] removing parameters from the header file

--- include/drivers/driving_distance/withPoints_dd_driver.h
+++ include/drivers/driving_distance/withPoints_dd_driver.h
@@ -51,22 +51,17 @@ extern "C" {
 
 
     void do_pgr_many_withPointsDD(
-            Edge_t      *edges,             size_t total_edges,
-            Point_on_edge_t *points_p,          size_t total_points,
-            Edge_t      *edges_of_points,   size_t total_edges_of_points,
+            Edge_t*,          size_t,
+            Point_on_edge_t*, size_t,
+            Edge_t*,          size_t,
 
-            int64_t  *start_pids_arr,    size_t s_len,
-            double distance,
+            int64_t*,         size_t,
+            double,
 
-            bool directed,
-            char driving_side,
-            bool details,
-            bool equiCost,
+            bool, char, bool, bool,
 
-            Path_rt **return_tuples, size_t *return_count,
-            char** log_msg,
-            char** notice_msg,
-            char ** err_msg);
+            Path_rt**, size_t*,
+            char**, char**, char **);
 
 
 #ifdef __cplusplus
