51be9c46316f4248aa35475236ce63084c4d753e
Aniket Agarwal <83874650+Aniket-debug@users.noreply.github.com>
2023-07-17 07:32:41 +0530

    GSoC 2023: Aniket Agarwal week 7 (#322)
    * fix typetest.pg
    
    * fixing ksp doc
    
    * removed v4ksp files and merged the content

--- include/drivers/yen/ksp_driver.h
+++ include/drivers/yen/ksp_driver.h
@@ -35,11 +35,13 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #   include <cstddef>
 using Edge_t = struct Edge_t;
 using Path_rt = struct Path_rt;
+using II_t_rt = struct II_t_rt;
 #else
 #   include <stddef.h>
 #   include <stdint.h>
 typedef struct Edge_t Edge_t;
 typedef struct Path_rt Path_rt;
+typedef struct II_t_rt II_t_rt;
 #endif
 
 
