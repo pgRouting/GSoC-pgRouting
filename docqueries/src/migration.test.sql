5b5fb508298b74647683ba04d974de24575557bb
Abhinav Jain <this.abhinav@gmail.com>
2023-07-31 03:52:20 +0530

    Updated Migration queries

--- docqueries/src/migration.test.sql
+++ docqueries/src/migration.test.sql
@@ -263,6 +263,14 @@ SELECT seq, path_seq, node, edge, cost, agg_cost FROM pgr_bdAstar(
 /* --bdastar5 */
 /* --withPointsKSP1 */
 SELECT * FROM pgr_withPointsKSP(
-  $$SELECT * FROM edges$$,
-  6, 10);
-/* --withPointsKSP1 */
+  $$SELECT id, source, target, cost, reverse_cost, x1, y1, x2, y2 FROM edges$$,
+  $$SELECT pid, edge_id, fraction, side from pointsOfInterest$$,
+  6, 10, 1,'r');
+
+/* --withPointsKSP2 */
+SELECT seq, path_seq, node, edge, cost, agg_cost FROM pgr_withPointsKSP(
+  $$SELECT id, source, target, cost, reverse_cost, x1, y1, x2, y2 FROM edges$$,
+  $$SELECT pid, edge_id, fraction, side from pointsOfInterest$$,
+  6, 10, 1,'r');
+
+/* --withPointsKSP3 */
