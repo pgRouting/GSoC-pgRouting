6dae9e1584a7b90f7d43275041c0dde17ff783e9
Aryan Gupta <guptaaryan1010@gmail.com>
2023-08-03 01:17:52 +0530

    [drivingdistance] updating  build extension update

--- sql/scripts/build-extension-update-files.pl
+++ sql/scripts/build-extension-update-files.pl
@@ -238,6 +238,9 @@ sub generate_upgrade_script {
             push @commands, drop_special_case_function("pgr_astar(text,bigint,bigint,boolean,integer,double precision,double precision)");
             push @commands, drop_special_case_function("pgr_drivingdistance(text,anyarray,double precision,boolean,boolean)");
             push @commands, drop_special_case_function("pgr_drivingdistance(text,bigint,double precision,boolean)");
+            push @commands, drop_special_case_function("pgr_bdastar(text,bigint,bigint,boolean,integer,numeric,numeric)");
+            push @commands, drop_special_case_function("pgr_bdastar(text,bigint,anyarray,boolean,integer,numeric,numeric)");
+            push @commands, drop_special_case_function("pgr_bdastar(text,anyarray,bigint,boolean,integer,numeric,numeric)");
         }
 
     }
