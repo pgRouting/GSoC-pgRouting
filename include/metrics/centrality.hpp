d1719a65a15f9b353a3dd14b0d1ad866c37bf14c
Arun Thakur <bedupako12mas@gmail.com>
2024-06-17 00:38:24 +0530

    [centrality] removed the johnson code

--- include/metrics/centrality.hpp
+++ include/metrics/centrality.hpp
@@ -1,5 +1,5 @@
 /*PGR-GNU*****************************************************************
-File: pgr_allpairs.hpp
+File: centrality.hpp
 
 Copyright (c) 2015 pgRouting developers
 Mail: project@pgrouting.org
@@ -25,10 +25,9 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
  ********************************************************************PGR-GNU*/
 
-// http://www.cs.rpi.edu/~musser/archive/2005/gsd/restricted/FloydWarshall/FloydWarshall.pdf
 
-#ifndef INCLUDE_ALLPAIRS_PGR_ALLPAIRS_HPP_
-#define INCLUDE_ALLPAIRS_PGR_ALLPAIRS_HPP_
+#ifndef INCLUDE_METRICS_CENTRALITY_HPP_
+#define INCLUDE_METRICS_CENTRALITY_HPP_
 #pragma once
 
 #include <deque>
@@ -56,30 +55,12 @@ template < class G > class Pgr_allpairs;
 // user's functions
 template < class G >
 void
-pgr_johnson(G &graph, std::vector< IID_t_rt> &rows) {
-    Pgr_allpairs< G > fn_johnson;
-    fn_johnson.johnson(graph, rows);
-}
-
-template < class G >
-void
 pgr_floydWarshall(G &graph, std::vector< IID_t_rt> &rows) {
     Pgr_allpairs< G > fn_floydWarshall;
     fn_floydWarshall.floydWarshall(graph, rows);
 }
 
 // for postgres
-template < class G >
-void
-pgr_johnson(
-        G &graph,
-        size_t &result_tuple_count,
-        IID_t_rt **postgres_rows) {
-    Pgr_allpairs< G > fn_johnson;
-    fn_johnson.johnson(graph, result_tuple_count, postgres_rows);
-}
-
-
 template < class G >
 void
 pgr_floydWarshall(
@@ -128,50 +109,6 @@ class Pgr_allpairs {
          CHECK_FOR_INTERRUPTS();
 
          boost::floyd_warshall_all_pairs_shortest_paths(
-                 graph.graph,
-                 matrix,
-                 weight_map(get(&pgrouting::Basic_edge::cost, graph.graph)).
-                 distance_combine(combine).
-                 distance_inf((std::numeric_limits<double>::max)()).
-                 distance_zero(0));
-
-         make_result(graph, matrix, rows);
-     }
-
-     void johnson(
-             G &graph,
-             size_t &result_tuple_count,
-             IID_t_rt **postgres_rows) {
-         std::vector< std::vector<double>> matrix;
-         make_matrix(graph.num_vertices(), matrix);
-         inf_plus<double> combine;
-
-         /* abort in case of an interruption occurs (e.g. the query is being cancelled) */
-         CHECK_FOR_INTERRUPTS();
-
-         boost::johnson_all_pairs_shortest_paths(
-                 graph.graph,
-                 matrix,
-                 weight_map(get(&pgrouting::Basic_edge::cost, graph.graph)).
-                 distance_combine(combine).
-                 distance_inf((std::numeric_limits<double>::max)()).
-                 distance_zero(0));
-
-         make_result(graph, matrix, result_tuple_count, postgres_rows);
-     }
-
-
-     void johnson(
-             G &graph,
-             std::vector< IID_t_rt> &rows) {
-         std::vector< std::vector<double>> matrix;
-         make_matrix(graph.num_vertices(), matrix);
-         inf_plus<double> combine;
-
-         /* abort in case of an interruption occurs (e.g. the query is being cancelled) */
-         CHECK_FOR_INTERRUPTS();
-
-         boost::johnson_all_pairs_shortest_paths(
                  graph.graph,
                  matrix,
                  weight_map(get(&pgrouting::Basic_edge::cost, graph.graph)).
@@ -263,4 +200,4 @@ class Pgr_allpairs {
 
 }  // namespace pgrouting
 
-#endif  // INCLUDE_ALLPAIRS_PGR_ALLPAIRS_HPP_
+#endif  // INCLUDE_METRICS_CENTRALITY_HPP_
