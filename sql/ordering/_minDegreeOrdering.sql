3c6d2ec671d2612d42403d05cbdfc6ab46900970
wifi:) <wifiBlack@gmail.com>
2025-06-25 16:01:34 +0800

    Change version to v4.0.0 in comment

--- sql/ordering/_minDegreeOrdering.sql
+++ sql/ordering/_minDegreeOrdering.sql
@@ -31,7 +31,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 -- _pgr_minDegreeOrdering
 ----------------------------
 
---v3.4.0
+--v4.0.0
 CREATE FUNCTION _pgr_minDegreeOrdering(
     TEXT,
     OUT seq BIGINT,
