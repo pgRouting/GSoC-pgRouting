0c05b54b2f35b49e117dc32ad8f7718355a0cfc2
sakirr05 <sakirahmed75531@gmail.com>
2026-05-26 16:18:54 +0000

    week1: add SQL skeleton for core numbers

---
+++ sql/kcore/pgr_coreNumbers.sql
@@ -0,0 +1,57 @@
+/*PGR-GNU*****************************************************************
+File: pgr_coreNumbers.sql
+
+Copyright (c) 2015-2026 pgRouting developers
+Mail: project@pgrouting.org
+
+Copyright (c) 2026 Sakir Ahmed
+Mail: sakirahmed75531@gmail.com
+
+----------------------------------------------------------------------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+********************************************************************PGR-GNU*/
+
+-------------------------
+-- pgr_coreNumbers
+-------------------------
+
+--v4.1
+
+CREATE FUNCTION pgr_coreNumbers(
+    TEXT, -- edges_sql
+
+    OUT node BIGINT,
+    OUT core_number INTEGER
+)
+RETURNS SETOF RECORD AS
+$BODY$
+
+    SELECT
+        NULL::BIGINT,
+        NULL::INTEGER
+    WHERE false;
+
+$BODY$
+LANGUAGE SQL VOLATILE STRICT;
+
+COMMENT ON FUNCTION pgr_coreNumbers(TEXT)
+IS
+'pgr_coreNumbers
+ - Experimental SQL skeleton for k-core decomposition
+ - Parameters:
+     - Edges SQL with columns: id, source, target, cost [,reverse_cost]
+';
