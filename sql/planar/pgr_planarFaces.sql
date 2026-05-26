5b3ef6fd8a084f940ef9fc6ff8ac0a193678ebb7
sakirr05 <sakirahmed75531@gmail.com>
2026-05-26 16:07:22 +0000

    fix: update copyright header

--- sql/planar/pgr_planarFaces.sql
+++ sql/planar/pgr_planarFaces.sql
@@ -1,10 +1,36 @@
 /*PGR-GNU*****************************************************************
+File: _makeBiconnectedPlanar.sql
 
-FILE: pgr_planarFaces.sql
+Copyright (c) 2015-2026 pgRouting developers
+Mail: project@pgrouting.org
 
-********************************************************************PGR-GNU*/
+Copyright (c) 2026 Mohit Rawat
+Mail: mohit25rawat at gmail.com
 
---v4.0
+------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ ********************************************************************PGR-GNU*/
+-------------------------
+-------------------------
+-- _planarFaces
+-------------------------
+-------------------------
+
+--v4.1
 
 CREATE FUNCTION pgr_planarFaces(
     TEXT, -- edges_sql
