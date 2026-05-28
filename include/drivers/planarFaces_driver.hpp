23b4a9bd5640c7a7594b44cfc34728a7365ea00b
sakir05 <sakirahmed690078@gmail.com>
2026-05-28 19:19:55 +0000

    fix the headers of every file structure

--- include/drivers/planarFaces_driver.hpp
+++ include/drivers/planarFaces_driver.hpp
@@ -1,8 +1,12 @@
 /*PGR-GNU*****************************************************************
+
 File: planarFaces_driver.hpp
 
-Copyright (c) 2026 pgRouting developers
+Copyright (c) 2015-2026 pgRouting developers
 Mail: project@pgrouting.org
+
+Copyright (c) 2026 Sakir Ahmed
+Mail: sakirahmed75531 at gmail.com
 
 ------
 
@@ -18,7 +22,7 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
 
 ********************************************************************PGR-GNU*/
 
