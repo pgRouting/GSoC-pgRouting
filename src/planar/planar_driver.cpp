82cc3827f7a716d85de7d17d5b9031efcb66acb1
sakirr05 <sakirahmed75531@gmail.com>
2026-06-10 18:39:00 +0530

    add shared planar process and driver implementation

---
+++ src/planar/planar_driver.cpp
@@ -0,0 +1,116 @@
+/*PGR-GNU*****************************************************************
+File: planar_driver.cpp
+
+Copyright (c) 2007-2026 pgRouting developers
+Mail: project@pgrouting.org
+
+Function's developer:
+Copyright (c) 2026 Sakir Ahmed
+Mail: sakirahmed75531 at gmail.com
+
+------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ ********************************************************************PGR-GNU*/
+
+#include "drivers/planar_driver.hpp"
+
+#include <vector>
+#include <string>
+#include <sstream>
+
+#include "cpp_common/pgdata_getters.hpp"
+#include "cpp_common/alloc.hpp"
+#include "cpp_common/assert.hpp"
+
+#include "planar/planarFaces.hpp"
+#include "cpp_common/base_graph.hpp"
+
+namespace pgrouting {
+namespace drivers {
+
+void
+do_planar(
+        const std::string &edges_sql,
+        Which which,
+
+        PlanarFace_rt *&return_tuples,
+        size_t &return_count,
+
+        std::ostringstream &log,
+        std::ostringstream &notice,
+        std::ostringstream &err) {
+    std::string hint = "";
+
+    try {
+        if (edges_sql.empty()) {
+            err << "Empty edges SQL";
+            return;
+        }
+
+        using pgrouting::pgget::get_edges;
+
+        hint = edges_sql;
+        auto edges = get_edges(edges_sql, true, true);
+        if (edges.empty()) {
+            notice << "No edges found";
+            log << hint;
+            return;
+        }
+        hint = "";
+
+        pgrouting::UndirectedGraph undigraph;
+        undigraph.insert_edges(edges);
+
+        switch (which) {
+            case PLANARFACES:
+                {
+                    pgrouting::functions::Pgr_planarFaces<pgrouting::UndirectedGraph> fn;
+                    auto results = fn.planarFaces(undigraph);
+                    auto count = results.size();
+
+                    if (count == 0) {
+                        notice << "No results found";
+                        return;
+                    }
+
+                    return_tuples = pgr_alloc(count, return_tuples);
+                    for (size_t i = 0; i < count; ++i) {
+                        return_tuples[i] = results[i];
+                    }
+                    return_count = count;
+                    break;
+                }
+            default:
+                break;
+        }
+    } catch (AssertFailedException &except) {
+        err << except.what();
+    } catch (const std::pair<std::string, std::string>& ex) {
+        err << ex.first;
+        log << ex.second;
+    } catch (const std::string &ex) {
+        err << ex;
+        log << hint;
+    } catch (std::exception &except) {
+        err << except.what();
+    } catch (...) {
+        err << "Caught unknown exception!";
+    }
+}
+
+}  // namespace drivers
+}  // namespace pgrouting
