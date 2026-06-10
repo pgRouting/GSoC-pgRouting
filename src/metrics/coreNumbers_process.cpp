dbf3592ca3071e6f95164aa9bf09e9140f3a97cb
sakirr05 <sakirahmed75531@gmail.com>
2026-06-10 20:40:50 +0530

    fix copyright template

--- src/metrics/coreNumbers_process.cpp
+++ src/metrics/coreNumbers_process.cpp
@@ -1,7 +1,7 @@
 /*PGR-GNU*****************************************************************
 File: coreNumbers_process.cpp
 
-Copyright (c) 2007-2026 pgRouting developers
+Copyright (c) 2026-2026 pgRouting developers
 Mail: project@pgrouting.org
 
 Function's developer:
