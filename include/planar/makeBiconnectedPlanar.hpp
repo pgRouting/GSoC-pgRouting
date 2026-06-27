1ef2e1ef50d312af59384b7e50377b79568001e3
Mohit Rawat <mohit25rawat@gmail.com>
2026-06-27 11:00:24 +0530

    Fix cpplint include warnings for algorithm and utility headers

--- include/planar/makeBiconnectedPlanar.hpp
+++ include/planar/makeBiconnectedPlanar.hpp
@@ -28,8 +28,10 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #define INCLUDE_PLANAR_MAKEBICONNECTEDPLANAR_HPP_
 #pragma once
 
+#include <algorithm>
 #include <map>
 #include <string>
+#include <utility>
 #include <vector>
 #include <cstdint>
 
