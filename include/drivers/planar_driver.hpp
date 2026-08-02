b2d2a65d50007998866a8493ea462658d12e113d
Mohit Rawat <mohit25rawat@gmail.com>
2026-08-02 17:13:31 +0530

    added bool parameter in do_planar

--- include/drivers/planar_driver.hpp
+++ include/drivers/planar_driver.hpp
@@ -44,6 +44,7 @@ namespace drivers {
 
 void do_planar(
         const std::string&,
+	bool,
         Which,
         II_t_rt*&, size_t&,
         std::ostringstream&, std::ostringstream&, std::ostringstream&);
