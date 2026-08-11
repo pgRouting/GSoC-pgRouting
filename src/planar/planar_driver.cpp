bdcc7fc5e505839aab6b44758e121e70ef0dab6b
Mohit Rawat <mohit25rawat@gmail.com>
2026-08-11 19:04:58 +0530

    fixed identation

--- src/planar/planar_driver.cpp
+++ src/planar/planar_driver.cpp
@@ -93,25 +93,25 @@ void do_planar(
         undigraph.insert_edges(edges);
 
         std::vector<II_t_rt> results;
-         if (directed) {
+        if (directed) {
             err << "planar_driver.cpp: Unknown function " << get_name(which)
                 << " for directed graph";
             return;
         } else {
-           switch (which) {
+            switch (which) {
                 case BICONNECTEDPLANAR:
                     {
-                    pgrouting::functions::Pgr_makeBiconnectedPlanar<UndirectedGraph>
-                        fn_makeBiconnectedPlanar;
-                    results = fn_makeBiconnectedPlanar.makeBiconnectedPlanar(undigraph);
-                    log << fn_makeBiconnectedPlanar.get_log();
-                }
-                break;
-            default:
-                err << "Unknown planar function " << get_name(which);
-                return;
+                        pgrouting::functions::Pgr_makeBiconnectedPlanar<UndirectedGraph>
+                            fn_makeBiconnectedPlanar;
+                        results = fn_makeBiconnectedPlanar.makeBiconnectedPlanar(undigraph);
+                        log << fn_makeBiconnectedPlanar.get_log();
+                    }
+                    break;
+                default:
+                    err << "Unknown planar function " << get_name(which);
+                    return;
+            }
         }
-     }
 
         std::sort(results.begin(), results.end(), [](const II_t_rt &a, const II_t_rt &b) {
             return a.d1 < b.d1 || (a.d1 == b.d1 && a.d2 < b.d2);
